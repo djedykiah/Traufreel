7b7d556a0c8fa38c40681fc6c91f886b7dadd08b
Yulian-Caesar <yulik19971997@gmail.com>
2018-10-25 18:38:59 +0300

    yulik done

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,25 @@
 //=include lib/sayHello.js
 $(function() {
 
+	$(".reviews-slider").slick({
+		arrows: false,
+		dots: true,
+		infinite: true
+	})
+
+	$(".reviews-slider__button").click(function (e) {
+			var block = $(this).parents(".reviews-slider__content");
+			block.toggleClass("reviews-slider__active").siblings().removeClass("reviews-slider__active");
+	});
+
+	$(".reviews-slick__prev").on("click", function () {
+        $('.reviews-slider').slick("slickPrev");
+        $(".reviews-slider__active").removeClass("reviews-slider__active");
+    });
+  $(".reviews-slick__next").on("click", function () {
+      $('.reviews-slider').slick("slickNext");
+      $(".reviews-slider__active").removeClass("reviews-slider__active")
+  });
+
+
 });
